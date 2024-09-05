bdbc4e8fc629e3291e211a7f049b86a4e2b3424c
Wesdell <wesdell17@gmail.com>
2024-09-05 10:54:56 -0500

    Add and style Auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,66 @@
+import { useCallback, useState } from 'react';
+
+import Input from '@/components/Input';
+
+export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login")
+
+  const toggleVariant = useCallback(
+    () => setVariant((currentVariant) => currentVariant === "login" ? "register" : "login"),
+    []
+  );
+
+  return (
+    <div className="relative h-full w-full bg-[url('/img/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/img/logo.svg" alt="Logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  label="Name"
+                  type="name"
+                  value={name}
+                  onChange={(e: React.FormEvent<HTMLInputElement>) => setName(e.currentTarget.value)}
+                />
+              )}
+              <Input
+                id="email"
+                label="Email"
+                type="email"
+                value={email}
+                onChange={(e: React.FormEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
+              />
+              <Input
+                id="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e: React.FormEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login" ? "First time using Netflix?" : "Already have an account?"}
+              <span className="text-white ml-1 hover:underline cursor-pointer" onClick={toggleVariant}>
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
