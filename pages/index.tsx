e4dd690bc7a5972bc0d001efcbfb872e418aa93f
Wesdell <wesdell17@gmail.com>
2024-09-10 10:39:13 -0500

    Add Navbar component to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
-import { Inter } from "next/font/google";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/Navbar";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-blue-500">Hello, World!</h1>
+      <Navbar />
     </main>
   );
 }
