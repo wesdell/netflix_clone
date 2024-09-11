14355b30e3f8e669cbff93a65628cbf0400c197b
Wesdell <wesdell17@gmail.com>
2024-09-11 10:43:14 -0500

    New endpoint to get a random movie from db

---
+++ pages/api/random.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import serverAuth from "@/lib/serverAuthentication";
+import prismadb from "@/lib/prismadb"
+
+export default async function hanlder(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req);
+
+    const movieCount = await prismadb.movie.count();
+    const randomIdx = Math.floor(Math.random() * movieCount);
+
+    const randomMovies = await prismadb.movie.findMany({
+      take: 1,
+      skip: randomIdx
+    });
+    return res.status(200).json(randomMovies[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
